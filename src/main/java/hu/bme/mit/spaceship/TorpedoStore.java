55375e4bfcc4dc91696f664b3d455069ae04bbc6
kerteszbogi <bogesz.kertesz@gmail.com>
2025-10-08 15:00:13 +0200

    Random generator bug fixed

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,9 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  // generate random
+  private Random random = new Random();
+
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
 
@@ -36,8 +39,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
-    double r = generator.nextDouble();
+    double r = random.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
